176e6aef7a40198a8f52b110453e94e4f1c8835d
Galuf1 <daniel.pizarro.farias@gmail.com>
2022-06-26 11:49:32 +0200

    from w3 schools

--- index.js
+++ index.js
@@ -1,23 +1,64 @@
-function addNewItem(evt){
-    evt.preventDefault();
-    let userInput = evt.target.elements[0]
-    console.log(userInput, userInput.value, evt)
-    var node = document.createElement('li');
-    node.appendChild(document.createTextNode(userInput.value))
-    document.querySelector("#list-items").appendChild(node)
+// create a "close" button and append it to each list item
+
+let myNodelist = document.getElementsByTagName("LI")
+// console.log(myNodelist)
+// let i
+
+for (i = 0; i < myNodelist.length; i++) {
+    let span = document.createElement("SPAN")
+    let txt = document.createTextNode("\u00D7")
+    span.className = "close"
+    console.log(span)
+    span.appendChild(txt)
+    myNodelist[i].appendChild(span)
+}
+// console.log(myNodelist)
+
+// click on a close button to hide the current list item
+
+let close = document.getElementsByClassName("close")
+// let i;
+for (i = 0; i < close.length; i++){
+    close[i].onclick = function() {
+        let div = this.parentElement
+        div.style.display= "none"
+    }
 }
 
-function strikeItem(evt){
-    evt.preventDefault();
-    let userInput = evt.target.elements[0]
-    let ul = document.getElementById("list-items")
-    let listItems = ul.getElementsByTagName('li')
-    for(let i = 0; i < listItems.length; i++){
-        let strItems = listItems[i].innerHTML
-        if(strItems === userInput.value){
-            let changedStr = listItems[i].style.textDecoration = "line-through"
-            console.log()
+// add a checked symbol when clicking on a list item
+
+const list = document.querySelector('ul')
+console.log(list)
+list.addEventListener('click', function(ev) {
+    if (ev.target.tagName === 'LI') {
+        ev.target.classList.toggle('checked')
+    }
+}, false)
+
+// create new list item when clicking on the add button
+
+function newElement() {
+    let li = document.createElement('li')
+    let inputValue = document.getElementById("myInput").value
+    let t = document.createTextNode(inputValue)
+    li.appendChild(t)
+    if (inputValue === '') {
+        alert("You must write something")
+    } else {
+        document.getElementById("myUL").appendChild(li)
+    }
+    document.getElementById("myInput").vslue = ""
+
+    let span = document.createElement("SPAN")
+    let txt = document.createTextNode("\u00D7")
+    span.className= "close"
+    span.appendChild(txt)
+    li.appendChild(span)
+
+    for (i = 0; i < close.length; i++){
+        close[i].onclick = function() {
+            let div = this.parentElement
+            div.style.display = "none"
         }
     }
-
 }
